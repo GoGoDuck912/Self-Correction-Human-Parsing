aca5ec70dae6d0188a6823d408d110b4451b216c
YunqiuXu <23272227+XuYunqiu@users.noreply.github.com>
2020-06-25 22:58:13 +0800

    Update parsing_fusion.sh

--- mhp_extension/scripts/parsing_fusion.sh
+++ mhp_extension/scripts/parsing_fusion.sh
@@ -3,5 +3,4 @@ python logits_fusion.py \
 --global_output_dir ./data/CIHP/global_pic_parsing \
 --msrcnn_output_dir ./data/CIHP/crop_pic_parsing \
 --gt_output_dir ./data/CIHP/crop_pic_parsing \
---mask_output_dir ./data/CIHP/crop_mask \
 --save_dir ./data/CIHP/mhp_fusion_parsing
